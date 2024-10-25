a6953e1f628f0ea36c49e61a8e514567c0275e8f
Yiming0576 <Yiming0576@gmail.com>
2024-10-25 09:58:36 -0400

    create a Java Project for Dealership

---
+++ src/main/java/com/pluralsight/dealership/DealerShipApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerShipApplication.class, args);
+	}
+
+}
